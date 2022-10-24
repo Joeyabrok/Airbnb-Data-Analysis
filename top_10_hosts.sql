601c6932fd07406f0b00fb5d0c60706dcf4fa4ec
Joeyabrok <92071801+Joeyabrok@users.noreply.github.com>
2022-10-23 20:15:06 -0700

    Update top_10_hosts.sql

--- top_10_hosts.sql
+++ top_10_hosts.sql
@@ -1,7 +1,7 @@
 
 /*
 The purpose of the query is to determine how much top 10 AirBnB earners in Amsterdam in September 2022 earned.
-The query retrieves the id, the lesting url,calculates how many days in the next 30 days are booked,the price and the projected revenue in the next 30 days.
+The query retrieves the id, the listing url,calculates how many days in the next 30 days are booked,the price and the projected revenue in the next 30 days.
 The price was in string format and had a dollar sign so some data manipulation is needed to get it to a suitable format.*/
 
 
